464cb15697a85909a51f752848cda54697d935ee
kvgvmi <oucheikhimane7@gmail.com>
2024-08-31 03:43:00 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+function popup() {
+    const popupContainer = document.createElement("div");
+    popupContainer.innerHTML = `
+    <div id="popupContainer">
+       <h1>New Note</h1>
+       <textarea id="note-text" placeholder="Enter your note ... "></textarea>
+       <div id="btn-container">
+          <button id="submiBtn" onclick="createNote()">Create Note</button>
+          <button id="closeBtn" onclick="closePopup()">Close</button>
+        </div>
+    </div>
+    `;
+    document.body.appendChild(popupContainer);
+
+}
+
+function closePopup() {
+    const popupContainer = document.getElementById("popupContainer");
+    if (popupContainer) {
+        popupContainer.remove();
+    }
+}
+
+function createNote() {
+    const popupContainer = document.getElementById('popupContainer');
+    const noteText = document.getElementById('note-text').value;
+    if (noteText.trim() !== '') {
+        const note = {
+        id: new Date().getTime(),
+        text: noteText
+        };
+
+        const existingNotes = JSON.parse(localStorage.getItem('notes')) || [];
+        existingNotes.push(note);
+
+        localStorage.setItem('notes', JSON.stringify(existingNotes));
+
+        document.getElementById('note-text').value = '';
+        
+    }
+}
